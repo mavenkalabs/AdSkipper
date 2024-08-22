b8e90dc6d4ec17b51f5086a3846e927dad887dcd
mavenkalabs <44828328+mavenkalabs@users.noreply.github.com>
2024-08-22 13:29:19 -0700

    Fix NPEs in production (#14)
    * Fix NPEs in production
    
    * Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mavenkalabs.adskipper"
         minSdk = 24
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.0.11"
+        versionCode = 12
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments += mapOf(
