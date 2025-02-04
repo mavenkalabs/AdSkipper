d2e5c4303dff0f9a6121822ae56214a7eba19cfa
mavenkalabs <mavenka.labs@gmail.com>
2025-02-04 10:46:22 -0800

    Mute Ads by default

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mavenkalabs.adskipper"
         minSdk = 24
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.0.12"
+        versionCode = 13
+        versionName = "1.0.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments += mapOf(
@@ -49,21 +49,21 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment:2.7.7")
-    implementation("androidx.navigation:navigation-ui:2.7.7")
-    implementation("androidx.annotation:annotation:1.8.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.navigation:navigation-fragment:2.8.6")
+    implementation("androidx.navigation:navigation-ui:2.8.6")
+    implementation("androidx.annotation:annotation:1.9.1")
     implementation("com.android.support.test:runner:1.0.2")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-core:5.9.0")
+    testImplementation("org.mockito:mockito-core:5.12.0")
 
     // Core library
     androidTestImplementation("androidx.test:core:1.6.1")
 
     // AndroidJUnitRunner and JUnit Rules
-    androidTestImplementation("androidx.test:runner:1.6.1")
+    androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("androidx.test:rules:1.6.1")
-    androidTestUtil("androidx.test:orchestrator:1.5.0")
+    androidTestUtil("androidx.test:orchestrator:1.5.1")
 
     // Assertions
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
