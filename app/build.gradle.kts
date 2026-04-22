ef523edf8d16df06d04d3aca7e8e4d7758e51bc3
mavenkalabs <mavenka.labs@gmail.com>
2026-04-21 21:10:41 -0700

    enable skip/mute in mini player

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,12 +48,12 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("androidx.navigation:navigation-fragment:2.9.4")
-    implementation("androidx.navigation:navigation-ui:2.9.4")
-    implementation("androidx.annotation:annotation:1.9.1")
+    implementation("androidx.navigation:navigation-fragment:2.9.7")
+    implementation("androidx.navigation:navigation-ui:2.9.7")
+    implementation("androidx.annotation:annotation:1.10.0")
     implementation("com.android.support.test:runner:1.0.2")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-core:5.19.0")
+    testImplementation("org.mockito:mockito-core:5.23.0")
 
     // Core library
     androidTestImplementation("androidx.test:core:1.7.0")
